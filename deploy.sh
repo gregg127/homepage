58b4ee6e3516365b0a55d9ecd1927f9c874d9aab
Grzegorz Gołębiowski <grzegorz.golebiowski127@gmail.com>
2025-05-20 12:26:46 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,7 @@ npm run clean
 
 log "Updating version in package.json..."
 sed -i '' "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" package.json
+npm install
 
 log "Building site..."
 npm run build
